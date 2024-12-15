ee7014c07195cfd532e1ac7b71c54d656a0df7cd
mrkhalnayak <141492925+mrkhalnayak@users.noreply.github.com>
2024-12-15 14:52:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const mongoConnectionString = `mongodb://${mongoUsername}:${mongoPassword}@${mon
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(""mongodb://momgoUser:mongoPassword@mongo:27017/mydatabase"", {
+  await mongoose.connect("mongodb://momgoUser:mongoPassword@mongo:27017/mydatabase", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
